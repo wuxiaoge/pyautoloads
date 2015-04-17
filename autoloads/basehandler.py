dc9a38412f79965a3246a8ed9d6d2d99b811715a
wuxiaoge <returnliu@gmail.com>
2015-04-17 17:45:51 +0800

    增加模板页面的缓存设在

--- autoloads/basehandler.py
+++ autoloads/basehandler.py
@@ -5,14 +5,15 @@ from autoloads import json_encode
 
 class _Template(object):
 
-    def __new__(cls,directories,module_directory,output_encoding='utf-8'):
+    def __new__(cls,directories,module_directory,output_encoding='utf-8',cache_enabled = False):
         if not hasattr(cls,'_instance'):
             cls._instance = object.__new__(cls)
-            cls._instance._init_template(directories,module_directory,output_encoding)
+            cls._instance._init_template(directories,module_directory,output_encoding,cache_enabled)
         return cls._instance
 
-    def _init_template(self,directories,module_directory,output_encoding):
-        self._lookup = TemplateLookup(directories=directories, module_directory=module_directory, output_encoding=output_encoding)
+    def _init_template(self,directories,module_directory,output_encoding,cache_enabled):
+        self._lookup = TemplateLookup(directories=directories, module_directory=module_directory, 
+                                      output_encoding=output_encoding,cache_enabled=cache_enabled)
 
     def render_template(self,template_name,**kwargs):
         _template = self._lookup.get_template(template_name)
@@ -27,7 +28,9 @@ class HTTPRequestHandler(tornado.web.RequestHandler):
         _directories      = self.settings.get('directories',['./templates'])
         _module_directory = self.settings.get('module_directory','./templates_modules')
         _output_encoding  = self.settings.get('output_encoding','utf-8')
-        self._template    = _Template(directories=_directories, module_directory=_module_directory, output_encoding=_output_encoding)
+        _cache_enabled    = self.settings.get('cache_enabled',False)
+        self._template    = _Template(directories=_directories, module_directory=_module_directory, 
+                                      output_encoding=_output_encoding, cache_enabled=_cache_enabled)
 
     def render_template(self,template_name,**kwargs):
         return self._template.render_template(template_name,**kwargs)
