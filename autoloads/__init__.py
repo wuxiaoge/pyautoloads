79db0600ed76e7bf28219d5b0fed4a9dbd354c1d
wujuguang <1154545932@qq.com>
2015-06-12 19:06:48 +0800

    修复兼容性旧项目

--- autoloads/__init__.py
+++ autoloads/__init__.py
@@ -7,3 +7,5 @@ from autoloads.basehandler import BaseRequestHandler
 
 from autoloads.utils.base import Entity, EntityHelper
 from autoloads.utils.dbutils import BuildFilter, RequestParser, EntityParser
+
+EntityOper = EntityHelper  # 为兼容重命名之前使用此项目
