877bc30f8a35becf3cadbf7682dffc2c8ae70646
wujuguang <1154545932@qq.com>
2015-06-18 16:12:14 +0800

    增加模板页面的缓存设在

--- autoloads/basehandler.py
+++ autoloads/basehandler.py
@@ -10,7 +10,7 @@ class _Template(object):
     """对第三方模板引擎的封装.
     """
 
-    def __new__(cls, directories, module_directory, output_encoding='utf-8'):
+    def __new__(cls, directories, module_directory, output_encoding='utf-8', cache_enabled=False):
         """在实例化时创建 "_lookup" 属性.
 
             :param directories:         mako模板文件存储位置, 列表形式: ['./templates']
@@ -20,11 +20,11 @@ class _Template(object):
 
         if not hasattr(cls, '_instance'):
             cls._instance = object.__new__(cls)
-            cls._instance._init_template(directories, module_directory, output_encoding)
+            cls._instance._init_template(directories, module_directory, output_encoding, cache_enabled)
 
         return cls._instance
 
-    def _init_template(self, directories, module_directory, output_encoding):
+    def _init_template(self, directories, module_directory, output_encoding, cache_enabled):
         """创建实例 "_lookup" 属性.
 
             :param directories:         mako模板文件存储位置, 列表形式: ['./templates']
@@ -34,7 +34,8 @@ class _Template(object):
 
         self._lookup = TemplateLookup(directories=directories,
                                       module_directory=module_directory,
-                                      output_encoding=output_encoding)
+                                      output_encoding=output_encoding,
+                                      cache_enabled=cache_enabled)
 
     def render_template(self, template_name, **kwargs):
         """使用指定数据, 渲染指定模板.
@@ -53,10 +54,12 @@ class HttpRequestHandler(tornado.web.RequestHandler):
         _directories = self.settings.get('directories', ['./templates'])
         _module_directory = self.settings.get('module_directory', './templates_modules')
         _output_encoding = self.settings.get('output_encoding', 'utf-8')
+        _cache_enabled = self.settings.get('cache_enabled', False)
 
         self._template = _Template(directories=_directories,
                                    module_directory=_module_directory,
-                                   output_encoding=_output_encoding)
+                                   output_encoding=_output_encoding,
+                                   cache_enabled=_cache_enabled)
 
     def render_template(self, template_name, **kwargs):
         """使用指定数据, 渲染指定模板.
