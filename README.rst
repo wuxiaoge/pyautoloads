487af54553b29d60b0d26f0b1cf79dcfd6e198c1
wujuguang <1154545932@qq.com>
2015-06-19 10:43:52 +0800

    增加setup发布的meta数据项

--- README.rst
+++ README.rst
@@ -8,3 +8,5 @@ python web and databases's tools.
 
 **pyautoloads** 是tornado web 简易开发工具包，支持数据库models自动生成加载，
 handler采用装饰器方式配置，并自动扫描handler并加载，使用方式见demo。
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'CHANGELOG.rst')).read()
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+zip_safe = not on_rtd
+
+version = '0.2'
 
 setup(
     name="pyautoloads",
-    version="0.2",
+    version=version,
+    description="hichao-test is a Tool that HTTP Test base on linux curl.",
+    long_description=README + '\n\n' + NEWS,
+    license='MIT License',
+    author='wuxiaoge',
+    author_email='returnliu@gmail.com',
+    keywords='pyautoloads',
+    url='https://github.com/wujuguang/pyautoloads.git',
     package_data={},
     packages=find_packages(),
     install_requires=["pymysql", "sqlalchemy", "tornado", "mako", "pipe"],
+    include_package_data=True,
+    platforms=["any"],
     zip_safe=False,
+    classifiers=[
+        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License'
+    ]
 )
